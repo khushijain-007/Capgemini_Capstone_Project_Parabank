8c95e027040b2488daab3e66d63678bfb9028687
khushijain-007 <kj9807887@gmail.com>
2026-06-16 22:13:32 +0530

    Updated Playwright config and gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
+  outputDir: 'screenshot/',
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -35,6 +36,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     // storageState: '.auth/storageState.json',
+    screenshot: 'on',
+    // video: 'on',
   },
 
   /* Configure projects for major browsers */
